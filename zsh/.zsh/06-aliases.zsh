627195f19c5c3d869389d20af453c8cb413c2226
Panat Taranat <panat.taranat@gmail.com>
2021-06-17 19:03:32 -0400

    add more aliases

--- zsh/.zsh/06-aliases.zsh
+++ zsh/.zsh/06-aliases.zsh
@@ -16,8 +16,29 @@ alias la="exa -la --group-directories-first --git"
 alias lm="exa -ls modified --group-directories-first --reverse --git"
 alias lmr="exa -ls modified --group-directories-first --git"
 
+# Edit configs
+alias zshrc="$EDITOR ~/.zshrc"
+alias zshalias="$EDITOR ~/.zsh/06-aliases.zsh"
+alias zshenv="$EDITOR ~/.zsh/01-environment.zsh"
+alias vimrc="$EDITOR ~/.vim/general.vim"
+alias vimplug="$EDITOR ~/.vim/plugins.vim"
+alias vimplugs="$EDITOR ~/.vim/plugin-settings.vim"
+alias vimui="$EDITOR ~/.vim/ui.vim"
+
 # Toys
 alias weather="curl wttr.in"
+alias disks='echo "╓───── m o u n t . p o i n t s"; \
+			echo "╙────────────────────────────────────── ─ ─ "; \
+			lsblk -a; echo ""; \
+			echo "╓───── d i s k . u s a g e";\
+			echo "╙────────────────────────────────────── ─ ─ "; \
+			df -h;'
+
+# GitHub CLI
+alias ghc="gh repo clone"
+
+# Files
+alias -s md="glow -p"
 
 # Search for aliases
 function what() {
@@ -26,5 +47,15 @@ function what() {
 		alias | grep --color=always "$1" | bat
 	else
 		alias | bat
+	fi
+}
+
+# Make folder then cd
+function mcd() {
+	if [ "$1" != "" ]
+	then
+		mkdir -p $1 ; cd $1
+	else
+		echo "Missing folder name"
 	fi
 }
